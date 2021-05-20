b415c07f7d76a1c82c2864e1470a0830ce73aec4
fchichorro <filipechichorro@gmail.com>
2021-05-21 01:27:43 +0300

    a few runs

--- pars_init_Linepithema_humile.m
+++ pars_init_Linepithema_humile.m
@@ -6,7 +6,7 @@ metaPar.model = 'hex';
 par.T_ref = 293.15;   free.T_ref = 0;   units.T_ref = 'K';        label.T_ref = 'Reference temperature'; 
 
 %% core primary parameters 
-par.T_A = 8000;       free.T_A   = 0;   units.T_A = 'K';          label.T_A = 'Arrhenius temperature'; 
+par.T_A = 8000;       free.T_A   = 1;   units.T_A = 'K';          label.T_A = 'Arrhenius temperature'; 
 par.z = 0.028524;     free.z     = 1;   units.z = '-';            label.z = 'zoom factor'; 
 par.F_m = 6.5;        free.F_m   = 0;   units.F_m = 'l/d.cm^2';   label.F_m = '{F_m}, max spec searching rate'; 
 par.kap_X = 0.8;      free.kap_X = 0;   units.kap_X = '-';        label.kap_X = 'digestion efficiency of food to reserve'; 
